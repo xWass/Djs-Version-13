adb7f88fd31e0bdda90fb85a8df34a76e81a2b2c
xWass <spectatorgaming02@gmail.com>
2021-12-28 21:41:20 -0500

    ping cmd update, userinfo update. more cmds coming

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with ping!'),
 	async execute(interaction) {
-		return interaction.reply('Pong!');
+		return interaction.reply(`Pong! ${client.ws.ping}ms.`);
 	},
 };
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -5,6 +5,9 @@ module.exports = {
 		.setName('user-info')
 		.setDescription('Display info about yourself.'),
 	async execute(interaction) {
-		return interaction.reply(`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}`);
+		interaction.followUp({
+			content: `Your username: ${interaction.user.username} \nYour ID: ${interaction.user.id} \nAccount creation date: ${interaction.user.createdAt}`,
+			ephemeral: false
+		});
 	},
 };
