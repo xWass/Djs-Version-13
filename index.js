9cbd89ee2cefe40b47bec95f4237985a16051388
xWass <spectatorgaming02@gmail.com>
2021-12-29 23:04:33 -0500

    fix pls i beg

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 		);
 		console.log('Successfully reloaded application (/) commands.');
 	} catch (error) {
-		console.error(error);
+		console.error(error)
 	}
 })();
 
