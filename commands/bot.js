fa2e01a95f27b6cdc46494034386fa98debbd85f
xWass <73602180+xWass@users.noreply.github.com>
2022-01-09 15:35:14 -0500

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -2,16 +2,17 @@ const {
     SlashCommandBuilder
 } = require('@discordjs/builders');
 const { Client } = require('discord.js');
-let days = Math.floor(interaction.client.uptime / 86400000);
-let hours = Math.floor(interaction.client.uptime / 3600000) % 24;
-let minutes = Math.floor(interaction.client.uptime / 60000) % 60;
-let seconds = Math.floor(interaction.client.uptime / 1000) % 60;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bot')
 		.setDescription('View bot information.'),
     async execute(interaction) {
+	    let days = Math.floor(interaction.client.uptime / 86400000);
+	    let hours = Math.floor(interaction.client.uptime / 3600000) % 24;
+	    let minutes = Math.floor(interaction.client.uptime / 60000) % 60;
+	    let seconds = Math.floor(interaction.client.uptime / 1000) % 60;
+
         await interaction.reply({
             content: `Ping: ${interaction.client.ws.ping}ms \nUptime: ${days}d ${hours}h ${minutes}m ${seconds}s \nMemory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`,
             components: null,
