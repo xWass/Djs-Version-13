9a6536e861d7d5f6e441ad466b38be69ca87f04a
xWass <spectatorgaming02@gmail.com>
2022-02-13 22:47:12 -0500

    ew cmd

---
+++ commands/unmute.js
@@ -0,0 +1,36 @@
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('mute')
+        .setDescription('Select a member and unmute them.')
+        .addUserOption(option => option
+            .setName('user')
+            .setDescription('The member to unmute.')),
+    async execute(interaction) {
+        const user = await interaction.options.getUser('user') || null
+        const mem = await interaction.options.getMember('user') || null
+        if (!interaction.member.permissions.has('MODERATE_MEMBERS'))
+            return void (await interaction.reply({
+                content: 'You do not have the `TIMEOUT_MEMBERS` permission.',
+                ephemeral: true
+            }));
+        if (!interaction.guild.me.permissions.has('MODERATE_MEMBERS'))
+            return void (await interaction.reply({
+                content: 'I do not have the `TIMEOUT_MEMBERS` permission.',
+                ephemeral: true
+            }));
+
+        if (!mem.moderatable)
+            return void (await interaction.reply({
+                content: 'I cannot unmute this user.',
+                ephemeral: true
+            }));
+        await mem.timeout(null)
+        await interaction.followUp({
+            content: `${user} unmuted.`,
+            ephemeral: false
+        });
+    }
+}
