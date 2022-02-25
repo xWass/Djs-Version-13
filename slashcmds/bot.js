82041750bde48b09a4fe6bf8edc1af67d71b26fd
xWass <spectatorgaming02@gmail.com>
2022-02-25 12:33:58 -0500

    yesnt

--- slashcmds/bot.js
+++ slashcmds/bot.js
@@ -8,15 +8,17 @@ module.exports = {
 		.setDescription('View bot information.'),
     async execute(interaction) {
 
-        const embed = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTimestamp()
-        .setDescription(`Ping: ${interaction.client.ws.ping}ms \nUptime: ${days}d ${hours}h ${minutes}m ${seconds}s \nMemory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`)
 
 	    let days = Math.floor(interaction.client.uptime / 86400000);
 	    let hours = Math.floor(interaction.client.uptime / 3600000) % 24;
 	    let minutes = Math.floor(interaction.client.uptime / 60000) % 60;
 	    let seconds = Math.floor(interaction.client.uptime / 1000) % 60;
+
+        const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTimestamp()
+        .setDescription(`Ping: ${interaction.client.ws.ping}ms \nUptime: ${days}d ${hours}h ${minutes}m ${seconds}s \nMemory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`)
+
 
         await interaction.reply({
             content: `Ping: ${interaction.client.ws.ping}ms \nUptime: ${days}d ${hours}h ${minutes}m ${seconds}s \nMemory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`,
