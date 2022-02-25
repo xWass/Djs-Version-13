885de32184cc98c6fae7611952cdce73e8763f77
xWass <spectatorgaming02@gmail.com>
2022-02-25 12:25:41 -0500

    push

--- index.js
+++ index.js
@@ -1,61 +1,118 @@
 const fs = require('fs');
+const chalk = require('chalk')
 const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const intents = new Intents();
+intents.add(
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MEMBERS
+);
+const client = new Client({ intents: intents, partials: ["MESSAGE", "REACTION"], allowedMentions: { parse: ["users"] } });
 require("dotenv").config();
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const { clientId, guildId } = require('./config.json');
+client.LegacyCommands = new Collection();
+client.SlashCommands = new Collection();
+const commandFiles = fs.readdirSync('./slashcmds').filter(file => file.endsWith('.js'));
+const legFiles = fs.readdirSync('./legacycmds').filter(file => file.endsWith('.js'));
 
+const { clientId, guildId, prefix } = require('./config.json');
+
+process.on('unhandledRejection', error => {
+    console.log(error);
+});
 
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const commands = [];
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`./slashcmds/${file}`);
+    commands.push(command.data.toJSON());
 }
+
+/*
+const databaseConnect = async () => {
+    const mongoClient = new MongoClient(process.env.MONGO);
+    await mongoClient.connect()
+    const database = mongoClient.db("database name")
+    client.db = database;
+    console.log("Connected to the database!")
+}
+*/
+
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 (async () => {
-	try {
-		console.log(commands.map(c => c.name))
-		console.log('Started refreshing application (/) commands.');
+    try {
+        console.log(chalk.yellowBright('Started refreshing application (/) commands.'));
 
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error)
-	}
+        await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: commands },
+        );
+        console.log(chalk.greenBright('Successfully reloaded application (/) commands.'));
+    } catch (error) {
+        console.error(error)
+    }
 })();
+
 client.on("ready", async () => {
-    client.user.setActivity(`to slash commands!`, { type: "LISTENING" })
-});
-client.once('ready', async () => {
-	for (const file of commandFiles) {
-		console.log(`Loaded ${file}!`);
-	}
-	console.log('Ready!')
+    //databaseConnect()
+	client.user.setActivity(`slash commands!`, { type: "LISTENING" })
 });
 
+client.once('ready', async () => {
+    for (const file of commandFiles) {
+        console.log(chalk.yellowBright('[SLASH COMMAND LOADED]') + ` ${file}`);
+    }
+    for (const file of legFiles) {
+        console.log(chalk.yellowBright('[LEGACY COMMAND LOADED]') + ` ${file}`);
+    }
+    console.log(chalk.greenBright('Ready!'))
+
+});
 for (const file of commandFiles) {
-    let command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
+    let command = require(`./slashcmds/${file}`);
+    client.SlashCommands.set(command.data.name, command);
 }
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
-	
-	if (!command) return;
+    const command = client.SlashCommands.get(interaction.commandName);
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+    if (!command) return;
+    console.log(chalk.yellowBright('[EVENT FIRED]') + ` interactionCreate with command ${interaction.commandName}`);
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        return interaction.reply({ content: `${error}`, ephemeral: true });
+    }
 });
+for (const file of legFiles) {
+    const cmd = require(`./legacycmds/${file}`);
+    client.LegacyCommands.set(cmd.name, cmd)
+}
 
+client.on("messageCreate", async (message) => {
+    if (message.author.bot) return
+    if (!message.content.startsWith("<@" + client.user.id + ">") && !message.content.startsWith("<@!" + client.user.id + ">") && !message.content.startsWith(prefix)) { return }
+    let split = message.content.split(" ");
+    let search = split[1]
+    if (message.content.startsWith(prefix)) search = split[0].slice(prefix.length)
+    let command = client.LegacyCommands.get(search) || client.LegacyCommands.find((cmd) => cmd.aliases && cmd.aliases.includes(search));
+    let i = 1;
+    console.log(chalk.yellowBright('[EVENT FIRED]') + ` messageCreate with content: ${message.content}`);
+
+    if (message.content.startsWith(prefix)) i++;
+    while (i <= 2) {
+        i++;
+        split.shift();
+    };
+    try {
+        await command.execute(client, message, split)
+    } catch (err) {
+        message.reply(err.toString());
+        console.log(chalk.redBright('[ERROR]') + ` ${err}`);
+    }
+});
 client.login(process.env.TOKEN);
