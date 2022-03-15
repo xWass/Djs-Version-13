7cf717dd33b0a8d898f4039362273be7b81fef71
xWass <spectatorgaming02@gmail.com>
2022-03-15 06:40:19 -0400

    maybe?

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ client.on("messageCreate", async (message) => {
         let split = message.content.split(" ");
         let search = split[1]
         if (message.content.startsWith(prefix)) search = split[0].slice(prefix.length)
-        let command = client.LegacyCommands.get(search) || client.LegacyCommands.find((cmd) => cmd.aliases || cmd.aliases.includes(search));
+        let command = client.LegacyCommands.get(search) || client.LegacyCommands.find((cmd) => cmd.aliases && cmd.aliases.includes(search));
         let i = 1;
         console.log(chalk.yellowBright('[EVENT FIRED]') + ` messageCreate with content: ${message.content}`);
 
