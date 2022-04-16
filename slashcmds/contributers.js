cf76b650bdd895e83742afeea31e15bcfecce6b9
bchurton <ben@churton.uk>
2022-04-16 22:44:04 +0100

    Update contributers.js

--- slashcmds/contributers.js
+++ slashcmds/contributers.js
@@ -9,7 +9,7 @@ module.exports = {
         console.log(chalk.greenBright('[EVENT ACKNOWLEDGED]') + ` interactionCreate with command contributers`);
         let embed = new MessageEmbed()
             .setTitle("Contributers")
-            .setDescription("**These are people who have contributed to Diomedes code:** \nDEEM#0001 (ID:887395123145609218)")
+            .setDescription("**These are people who have contributed to Diomedes code:** \nDEEM#0001 (ID:887395123145609218)\nbig.bun#6969 (471409054594498561)")
             .setFooter("Say thanks to the contributers :)")
 
         interaction.reply({ embeds: [embed] });
