96d7f69e6d4213641f91677846376aecabaa28fe
xWass <73602180+xWass@users.noreply.github.com>
2022-05-16 23:25:05 -0400

    smol rng custom id

--- slashcmds/suggest.js
+++ slashcmds/suggest.js
@@ -1,14 +1,26 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, Modal, MessageActionRow, TextInputComponent } = require('discord.js');
 const chalk = require('chalk');
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateCaptchaString() {
+    let str = '';
+
+    for (let i = 0; i < 6; i++)
+        str += `${letters[Math.floor(Math.random() * letters.length)]} `;
+
+    return str;
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('suggest')
         .setDescription('Submit a suggestion'),
 
     async execute(interaction) {
+        const customid = generateCaptchaString();
         const modal = new Modal()
-            .setCustomId('suggestt')
+            .setCustomId(customid)
             .setTitle('Suggestions');
 
         // text input components
@@ -28,7 +40,7 @@ module.exports = {
 
         await interaction.showModal(modal);
 
-        const filter = (interaction) => interaction.customId === 'suggestt';
+        const filter = (interaction) => interaction.customId === 'suggest';
 
         interaction.awaitModalSubmit({ filter, time: 150_000 })
             .then(async (modal) => {
