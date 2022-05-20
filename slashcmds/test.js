0ceb34c903cb1d588480a6f97ea6248f8734a8b8
xWass <73602180+xWass@users.noreply.github.com>
2022-05-19 22:35:53 -0400

    test subcommand

---
+++ slashcmds/test.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, Modal, MessageActionRow, TextInputComponent } = require('discord.js');
+const chalk = require('chalk');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('suggest')
+        .setDescription('Submit a suggestion')
+        .addSubcommand(sub => sub
+            .setName('user')
+            .setDescription('test'))
+			.addUserOption(option => option.setName('target').setDescription('The user'))
+            .addStringOption(option => option.setName('str').setDescription('The string'))
+        .addSubcommand(sub => sub
+            .setName('test')
+            .setDescription('test')
+            .addUserOption(option => option.setName('target2').setDescription('The user'))
+            .addStringOption(option => option.setName('str2').setDescription('The string'))
+        ),
+
+    async execute(interaction) {
+        console.log(chalk.greenBright('[EVENT ACKNOWLEDGED]') + ` interactionCreate with command test`);
+        interaction.reply({ content: "just a test command", ephemeral: true });
+    }
+}
